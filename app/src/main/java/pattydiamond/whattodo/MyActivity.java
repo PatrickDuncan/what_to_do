936eee324054406d7e5f8a18553ba14b16e83490
Patrick Duncan <patrick.duncan@outlook.com>
2015-05-09 20:37:55 -0400

    Implemented notifications

--- app/src/main/java/pattydiamond/whattodo/MyActivity.java
+++ app/src/main/java/pattydiamond/whattodo/MyActivity.java
@@ -1,49 +1,87 @@
 package pattydiamond.whattodo;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
 public class MyActivity extends ActionBarActivity {
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    EditText text;
+    NotificationCompat.Builder mBuilder;
+    NotificationManager mNotifyMgr;
+    int mNotificationId = 001;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        text = (EditText) findViewById(R.id.edit_message);
+        loadSavedPreferences();
+        mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("What To Do")
+                .setContentText(text.getText().toString())
+                .setOngoing(true);
+        Intent resultIntent = new Intent(this, MyActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MyActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent
+                (0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        text.addTextChangedListener(textChange);
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+
+    private void loadSavedPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        text.setText(sharedPreferences.getString("string_text", ""));
+    }
+
+    private void savePreferences(String key, String value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+    public void saveData(){
+        savePreferences("string_text", text.getText().toString());
+    }
+    @Override
+    public void onBackPressed() {
+        saveData();
+        super.onBackPressed();
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private final TextWatcher textChange = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+        public void afterTextChanged(Editable s) {
+            mBuilder.setContentText(s.toString());
+            mNotifyMgr.notify(mNotificationId, mBuilder.build());
+        }
+    };
 }
