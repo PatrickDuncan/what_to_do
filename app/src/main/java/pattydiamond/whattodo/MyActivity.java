92253eeefdc2132e16ffef501641e177b45a0b51
Patrick Duncan <patrick.duncan@outlook.com>
2015-05-11 06:47:16 -0400

    Multiple tasks

--- app/src/main/java/pattydiamond/whattodo/MyActivity.java
+++ app/src/main/java/pattydiamond/whattodo/MyActivity.java
@@ -14,29 +14,42 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Html;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MyActivity extends ActionBarActivity {
 
-    EditText text;
+    EditText[] Text = new EditText[3];
     NotificationCompat.Builder mBuilder;
     NotificationManager mNotifyMgr;
     int mNotificationId = 001;
+    String content, bullet = Html.fromHtml("&#8226").toString() + " ";
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        text = (EditText) findViewById(R.id.edit_message);
+        findMaxLength();
+        Text[0] = (EditText) findViewById(R.id.box_1);
+        Text[1] = (EditText) findViewById(R.id.box_2);
+        Text[2] = (EditText) findViewById(R.id.box_3);
         loadSavedPreferences();
+        content = bullet + Text[0].getText().toString();
         mBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle("What To Do")
-                .setContentText(text.getText().toString())
-                .setOngoing(true);
+                .setContentTitle("What To Do?")
+                .setContentText(Text[0].getText().toString())
+                .setOngoing(true)
+                .setShowWhen(false)
+                .setPriority(2);
+        for (int i=1; i<Text.length; i++){
+            content = content + "\n" + bullet + Text[i].getText().toString();
+        }
+        mBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(content));
         Intent resultIntent = new Intent(this, MyActivity.class);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         stackBuilder.addParentStack(MyActivity.class);
@@ -45,13 +58,26 @@ public class MyActivity extends ActionBarActivity {
                 (0, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(resultPendingIntent);
         mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        text.addTextChangedListener(textChange);
+        Text[0].addTextChangedListener(textChange);
+        Text[1].addTextChangedListener(textChange);
+        Text[2].addTextChangedListener(textChange);
         mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+
+    private void findMaxLength() {
+
+    }
+
+    private boolean isTooLarge (TextView text, String newText) {
+        float textWidth = text.getPaint().measureText(newText);
+        return (textWidth >= text.getMeasuredWidth ());
     }
 
     private void loadSavedPreferences() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        text.setText(sharedPreferences.getString("string_text", ""));
+        Text[0].setText(sharedPreferences.getString("string_text1", ""));
+        Text[1].setText(sharedPreferences.getString("string_text2", ""));
+        Text[2].setText(sharedPreferences.getString("string_text3", ""));
     }
 
     private void savePreferences(String key, String value) {
@@ -60,28 +86,29 @@ public class MyActivity extends ActionBarActivity {
         editor.putString(key, value);
         editor.commit();
     }
-    public void saveData(){
-        savePreferences("string_text", text.getText().toString());
+    public void saveData() {
+        savePreferences("string_text1", Text[0].getText().toString());
+        savePreferences("string_text2", Text[1].getText().toString());
+        savePreferences("string_text3", Text[2].getText().toString());
     }
-    @Override
-    public void onBackPressed() {
-        saveData();
-        super.onBackPressed();
-    }
-
-
-
 
     private final TextWatcher textChange = new TextWatcher() {
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         }
 
+
         public void onTextChanged(CharSequence s, int start, int before, int count) {
         }
 
         public void afterTextChanged(Editable s) {
-            mBuilder.setContentText(s.toString());
+            content = bullet + Text[0].getText().toString();
+            mBuilder.setContentText(content);
+            for (int i=1; i<Text.length; i++){
+                content = content + "\n" + bullet + Text[i].getText().toString();
+            }
+            mBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(content));
             mNotifyMgr.notify(mNotificationId, mBuilder.build());
+            saveData();
         }
     };
 }
